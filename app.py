24a51e08207d2a0e07b951720d73b2f829517263
Adel Hashem <96683697+AdelHashem@users.noreply.github.com>
2023-02-05 22:28:36 +0200

    Handle Errors

--- app.py
+++ app.py
@@ -10,10 +10,18 @@ mxm = MXM()
 def index():
     if request.method == 'POST':
         link = request.form['link']
-        isrc = sp.get_isrc(link)
+        try:
+            isrc = sp.get_isrc(link) if len(link) > 12 else link
+        except:
+            return "Wrong Spotify Link Or Wrong ISRC"
+            
         mxmLinks = mxm.Track_links(isrc)
-        return render_template('index.html', id=mxmLinks[0], track_link=mxmLinks[1].split("?")[0], album_link=mxmLinks[2])
+        if isinstance(mxmLinks, str):
+            return mxmLinks
+
+        return render_template('index.html', id=mxmLinks[0], isrc = isrc,track_link=mxmLinks[1].split("?")[0], album_link=mxmLinks[2])
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
