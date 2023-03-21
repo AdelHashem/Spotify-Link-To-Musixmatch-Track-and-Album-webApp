2934260e148ac644fe7239216f1e44d6e18b5163
Adel Hashem <96683697+AdelHashem@users.noreply.github.com>
2023-03-21 11:59:34 +0200

    use query parameter instead of post

--- app.py
+++ app.py
@@ -6,25 +6,25 @@ app = Flask(__name__)
 sp = Spotify()
 mxm = MXM()
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
-    if request.method == 'POST':
-        link = request.form['link']
+    link = request.args.get('link')
+    if link:
         try:
             if(len(link) < 12): return render_template('index.html', tracks_data= ["Wrong Spotify Link Or Wrong ISRC"])
             isrcs = sp.get_isrc(link) if len(link) > 12 else [{"isrc": link, "image": None}]
             #if isinstance(mxmLinks, list):
                 #return "Fetching data failed!"
         except Exception as e:
-            return str(e)
+            return render_template('index.html', tracks_data= [str(e)])
             
         mxmLinks = mxm.Tracks_Data(isrcs)
         if isinstance(mxmLinks, str):
             return mxmLinks
 
         return render_template('index.html', tracks_data= mxmLinks)
-    return render_template('index.html')
 
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
