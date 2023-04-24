4fc52d0abdca407580969e1fc14dbc1c803795c3
Adel Hashem <96683697+AdelHashem@users.noreply.github.com>
2023-04-24 16:56:58 +0200

    try to import new tracks

--- mxm.py
+++ mxm.py
@@ -6,13 +6,14 @@ class MXM:
 
     def __init__(self, key=None):
         self.key = key or self.DEFAULT_KEY
+        self.session = requests.Session()
 
     def change_key(self, key):
         self.key = key
 
-    def track_get(self, isrc) -> dict:
+    def track_get(self, isrc = None, commontrack_id = None) -> dict:
         url = f"{self.BASE_URL}track.get"
-        params = {"track_isrc": isrc, "apikey": self.key}
+        params = {"track_isrc": isrc, "commontrack_id":commontrack_id, "apikey": self.key}
         response = self.session.get(url, params=params)
         try:
             response.raise_for_status()
@@ -27,14 +28,39 @@ class MXM:
             elif status_code == 403:
                 return "You are not authorized to perform this operation"
             elif status_code == 404:
-                return """The requested resource was not found. The track hasn't been imported yet.
-                \nAdd the track to a playlist or add the Album to your library then run it via the app."""
+                return 404
+                #return "The requested resource was not found. The track hasn't been imported yet."
+        except requests.exceptions.HTTPError as e:
+            return f"HTTP error occurred: {e}"
+        except:
+            return "Error in MXM API"
+        
+    def matcher_track(self,sp_id):
+        url = f"{self.BASE_URL}matcher.track.get"
+        params = {"track_spotify_id": sp_id, "q_album": "None", "q_artist": "None", "q_track": "None", "apikey": self.key}
+        response = self.session.get(url, params=params)
+        try:
+            response.raise_for_status()
+            data = response.json()
+            status_code = data["message"]["header"]["status_code"]
+            if status_code == 200:
+                return data
+            elif status_code == 401:
+                return "Invalid or missing API key"
+            elif status_code == 402:
+                return "Usage limit has been reached. Try another API key."
+            elif status_code == 403:
+                return "You are not authorized to perform this operation"
+            elif status_code == 404:
+                return 404
+                #return "The requested resource was not found. The track hasn't been imported yet."
         except requests.exceptions.HTTPError as e:
             return f"HTTP error occurred: {e}"
         except:
             return "Error in MXM API"
 
-    def Track_links(self,isrc):
+        
+    def Track_links(self, isrc):
         track = self.track_get(isrc)
         try:
             id = track["message"]["body"]["track"]["commontrack_id"]
@@ -44,17 +70,30 @@ class MXM:
         album_id = track["message"]["body"]["track"]["album_id"]
 
         album_url = f"https://www.musixmatch.com/album/id/{album_id}"
-        return [id,track_url,album_url]
-    
+        return [id, track_url, album_url]
 
-
-    def Tracks_Data(self,iscrcs):
-        self.session = requests.Session()
+    def Tracks_Data(self, iscrcs):
         tracks = []
-        if "isrc" not in iscrcs[0]: return iscrcs
+        Limit = 5
+        import_count = 0
+        if "isrc" not in iscrcs[0]:
+            return iscrcs
         for i in iscrcs:
             track = self.track_get(i["isrc"])
-            time.sleep(.1)
+
+            # try to import the track
+            if track == 404:
+                
+                if import_count < 2:
+                    import_count +=1
+                    self.matcher_track(i["track"]["id"])
+                    time.sleep(1)
+                track = self.track_get(i["isrc"])
+                if track == 404:
+                    track = self.matcher_track(i["track"]["id"])
+            if track == 404:
+                track = "The track hasn't been imported yet. Try one more time after a minute (tried to import it using matcher call)."
+            
             try:
                 track = track["message"]["body"]["track"]
                 track["isrc"] = i["isrc"]
@@ -62,6 +101,5 @@ class MXM:
                 tracks.append(track)
             except (TypeError, KeyError):
                 tracks.append(track)
-        self.session.close()
+            time.sleep(.1)
         return tracks
-        
